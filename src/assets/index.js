fb2dd62d40dcc112a296da7015b2b291e8955e35
JaredIng87 <jaredingersoll14@gmail.com>
2023-03-25 23:40:10 -0400

    experience timeline

--- src/assets/index.js
+++ src/assets/index.js
@@ -23,8 +23,8 @@ import threejs from "./tech/threejs.svg";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import rutgers from "./company/rutgers.png"
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -54,9 +54,9 @@ export {
   threejs,
   meta,
   shopify,
-  starbucks,
   tesla,
   carrent,
   jobit,
   tripguide,
+  rutgers,
 };
