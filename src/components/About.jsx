8e671d107618ea4afc442cc115e9a15badb32303
JaredIng87 <jaredingersoll14@gmail.com>
2023-03-29 19:33:49 -0400

    about me edit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -42,7 +42,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        I am 35 years old, and i live in Trenton, NJ. i've spent the last 10 years working in the plumbing field, and decided to make a carr=eer change into web development.
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
