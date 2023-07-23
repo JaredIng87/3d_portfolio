09913cc6e71ecd9d3a2388d8d970e14ec3b233b4
JaredIng87 <jaredingersoll14@gmail.com>
2023-07-23 08:33:11 -0400

    remove unused code

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,4 @@
 import logo from "./logo.svg";
-import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
@@ -26,10 +22,6 @@ import coffee from "./coffee.png";
 
 export {
   logo,
-  backend,
-  creator,
-  mobile,
-  web,
   github,
   menu,
   close,
